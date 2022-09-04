2fcd4ef4d85483843b4f2c79b2c7d9b3d1cb0039
DarkIslandDev <DarkIslandDev@gmail.com>
2022-09-05 00:51:35 +0700

    da

--- src/Sitemap.php
+++ src/Sitemap.php
@@ -35,14 +35,19 @@ class Sitemap extends AbstractSitemap
      */
     public function add($loc, $lastmod = null, $changefreq = null, $priority = null)
     {
+        $loc     = $this->escapeString($loc);
+        $lastmod = !is_null($lastmod) ? $this->formatDate($lastmod) : null;
+
+        return $this->addUrlToDocument(compact('loc', 'lastmod', 'changefreq', 'priority'));
+
         $dir = "upload";
         if(!is_dir($dir))
         {
             mkdir($dir,0777,true);
-            $loc     = $this->escapeString($loc);
-            $lastmod = !is_null($lastmod) ? $this->formatDate($lastmod) : null;
-
-            return $this->addUrlToDocument(compact('loc', 'lastmod', 'changefreq', 'priority'));
+            if (!file_exists($loc))
+            {
+                $fIn = fopen($dir.$loc, 'w+');
+            }
         }
     }
 }
